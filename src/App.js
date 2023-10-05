3d868a6cb4ec7f121a067e9e0852c0a1eba0f8bb
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-05 16:48:37 -0300

    add imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import React from 'react'
-import './App.css'
+import React, {useEffect} from 'react';
+import  { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import './App.css';
 
 const App = () => {
     return (
