b04a85312c43b922811f7f91e4738471f8ff535d
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-05 10:10:46 -0300

    fix ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { ReactDOM } from "react-dom";
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // (o el nombre de tu componente principal)
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
