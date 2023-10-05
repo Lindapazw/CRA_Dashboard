77d896e1ce628a46014f9626c6a10cf0a1ebd470
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-05 11:57:36 -0300

    add tailwind in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './index.css'
 import App from './App'; // (o el nombre de tu componente principal)
 
 ReactDOM.render(<App />, document.getElementById('root'));
