f6edbd9edf1ca34e57e8bb81b4284a1aaa566073
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-10 16:17:05 -0300

    add Navbar

--- src/App.js
+++ src/App.js
@@ -32,6 +32,12 @@ const App = () => {
                         </div>
                     )}
 
+                    <div className={ `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72 w-full' : 'w-full flex-2'}`}>
+                        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+                            Navbar
+                        </div>
+                    </div>
+
                 </div>
             </BrowserRouter>
         </div>
