2bb0152f9c9c9fdafeb90d207b1eb6d37b350cb5
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-11 14:06:48 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div className='flex relative dark:bg-main-dark-bg'>
-                    
+
+                    {/* setting button */}
                     <div className='fixed right-4 bottom-4' style={{zIndex:'1000'}}>
                         <TooltipComponent content="setting" position='Top'>
                             <button type='button' className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white' style={{background:'blue', borderRadius:'50%'}}>
@@ -22,6 +23,7 @@ const App = () => {
                         </TooltipComponent>
                     </div>
 
+                    {/* Sidebar */}
                     {activeMenu ? (
                         <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
                             Sidebar
@@ -32,10 +34,37 @@ const App = () => {
                         </div>
                     )}
 
+                    {/* Navbar */}
                     <div className={ `dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72 w-full' : 'w-full flex-2'}`}>
                         <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
                             Navbar
                         </div>
+                    </div>
+
+                    <div>
+                        <Routes>
+                            {/* Dashboard */}
+                            <Route path='/' element='EComerce'></Route>
+                            <Route path='/ecomerce' element='EComerce'></Route>
+                            {/* Pages */}
+                            <Route path='/orders' element='Orders'></Route>
+                            <Route path='/employees' element='Employees'></Route>
+                            <Route path='/customers' element='Custumers'></Route>
+                            {/* Apps */}
+                            <Route path='/kanban' element='Kanban'></Route>
+                            <Route path='/editor' element='Editor'></Route>
+                            <Route path='/calendar' element='Calendar'></Route>
+                            <Route path='/color-picker' element='ColorPicker'></Route>
+                            {/* Charts */}
+                            <Route path='/line' element='Line'></Route>
+                            <Route path='/area' element='Area'></Route>
+                            <Route path='/bar' element='Bar'></Route>
+                            <Route path='/pie' element='Pie'></Route>
+                            <Route path='/Financial' element='financial'></Route>
+                            <Route path='/color-mapping' element='ColorMapping'></Route>
+                            <Route path='/pyramid' element='Pyramid'></Route>
+                            <Route path='/stacked' element='Stacked'></Route>
+                        </Routes>
                     </div>
 
                 </div>
