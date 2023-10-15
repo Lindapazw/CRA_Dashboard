2b7cea6ee7e63f3f70be31bbbe25586c9dec8830
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-15 18:28:54 -0300

    fiz TooltipComponent in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,9 +14,18 @@ import { useStateContext } from '../contexts/ContextProvider';
 
 
 const NavButton = ({title, customFunc, icon, color, dotColor}) => (
-    <TooltipComponent content={title} position='BottomCenter'>
-        <button type='button' onClick={customFunc} style={{color}} className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
-            <span style={{background:dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'>{icon}</span>
+    <TooltipComponent content={title} position="BottomCenter">
+        <button
+        type="button"
+        onClick={() => customFunc()}
+        style={{ color }}
+        className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+        >
+        <span
+            style={{ background: dotColor }}
+            className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+        />
+        {icon}
         </button>
     </TooltipComponent>
 )
