08a40ddd5af1f6d946ce94c51f41d26ec02e74bb
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-15 18:06:00 -0300

    fix Notification

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -5,7 +5,7 @@ export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
 export { default as Cart } from './Cart';
 export { default as Chat } from './Chat';
-export { default as Notification } from './Notification';
+export { default as Notification } from './Notifications';
 export { default as UserProfile } from './UserProfile';
 export { default as SparkLine } from './Charts/SparkLine';
 export { default as LineChart } from './Charts/LineChart';
