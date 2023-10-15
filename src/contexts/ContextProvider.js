b030126a1d449b97c768199a4453ffe0ae5cbf26
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-15 18:23:54 -0300

    add screenSize

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -12,6 +12,8 @@ const initialState = {
 export const ContextProvider = ({children}) => {
     const [activeMenu, setActiveMenu] = useState (true);
     const [isClicked, setIsClicked] = useState (initialState);
+    const [screenSize, setScreenSize] = useState(undefined)
+
     const handleClick = ( clicked ) => {
         setIsClicked({...initialState, [clicked]: true });
     }
@@ -21,7 +23,9 @@ export const ContextProvider = ({children}) => {
             setActiveMenu,
             isClicked, 
             setIsClicked,
-            handleClick
+            handleClick,
+            screenSize, 
+            setScreenSize
         }}>
             {children}
         </StateContext.Provider>
