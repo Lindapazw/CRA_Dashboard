6e16b414a279eced642580d46e305ce7be13b305
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-15 11:44:26 -0300

    add isClicked useState

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,10 +11,13 @@ const initialState = {
 
 export const ContextProvider = ({children}) => {
     const [activeMenu, setActiveMenu] = useState (true);
+    const [isClicked, setIsClicked] = useState (initialState);
     return (
         <StateContext.Provider value={{ 
             activeMenu,
-            setActiveMenu
+            setActiveMenu,
+            isClicked, 
+            setIsClicked,
         }}>
             {children}
         </StateContext.Provider>
