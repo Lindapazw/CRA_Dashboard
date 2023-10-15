d370fd917e085edcd49c15d8ab1b14fa3708395c
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-15 11:42:02 -0300

    add profile style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,9 @@ import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import avatar from '../data/avatar.jpg';
 import { Cart, Chat, Notification, UserProfile } from '.';
+// ContextProvider
 import { useStateContext } from '../contexts/ContextProvider';
+
 
 const NavButton = ({title, customFunc, icon, color, dotColor}) => (
     <TooltipComponent content={title} position='BottomCenter'>
@@ -38,6 +40,30 @@ const Navbar = () => {
                     color="blue" 
                     icon={<FiShoppingCart/>} 
                 />
+                <NavButton 
+                    title="Chat" 
+                    dotColor="#03c9d7"
+                    customFunc={() => handleClick('chat')} 
+                    color="blue" 
+                    icon={<BsChatLeft/>} 
+                />
+                <NavButton 
+                    title="Notifications" 
+                    dotColor="#03c9d7"
+                    customFunc={() => handleClick('notifications')} 
+                    color="blue" 
+                    icon={<RiNotification3Line/>} 
+                />
+                <TooltipComponent content="Profile" position='BottomCenter'>
+                    <div className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg' onClick={() => handleClick('userProfile')}>
+                        <img className='rounded-full w-8 h-8' src={avatar} alt="avatar" />
+                        <p>
+                            <span className='text-gray-400 text-14'>Hi,</span> {''}
+                            <span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
+                        </p>
+                        <MdKeyboardArrowDown className='text-gray-400 text-14'/>
+                    </div>
+                </TooltipComponent>
             </div>
         </div>
     )
