0e1a2e6fdf9e41f0b00812dbea5db357f9534a8f
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-16 10:44:58 -0300

    update button style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const Button = ({bgColor, color, size, text, borderRadius}) => {
+import { useStateContext } from '../contexts/ContextProvider';
+
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
+    const { setIsClicked, initialState } = useStateContext();
+
     return (
         <button
-            type='button'
-            style={{ backgroundColor:bgColor, color, borderRadius}}
-            className={`text-${size} p-3 hover:drop-shadow-xl`}
+        type="button"
+        onClick={() => setIsClicked(initialState)}
+        style={{ backgroundColor: bgColor, color, borderRadius }}
+        className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
         >
-            {text}
+        {icon} {text}
         </button>
-    )
-}
+    );
+};
 
-export default Button
-
+export default Button;
