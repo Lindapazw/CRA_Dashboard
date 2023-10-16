ae0c2e0fea0c344ea626c41469629cd725caeb19
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-16 10:31:04 -0300

    add button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({bgColor, color, size, text, borderRadius}) => {
     return (
-        <div>Button</div>
+        <button
+            type='button'
+            style={{ backgroundColor:bgColor, color, borderRadius}}
+            className={`text-${size} p-3 hover:drop-shadow-xl`}
+        >
+            {text}
+        </button>
     )
 }
 
 export default Button
+
