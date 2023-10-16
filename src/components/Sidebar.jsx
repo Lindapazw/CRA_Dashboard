2c0770dc3af9fe4cda23548ab436216c958527a9
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-16 11:43:10 -0300

    change logo name

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,7 +25,7 @@ const Sidebar = () => {
             <>
             <div className="flex justify-between items-center">
                 <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-                <SiShopware /> <span>Shoppy</span>
+                <SiShopware /> <span>Tu logo</span>
                 </Link>
                 <TooltipComponent content="Menu" position="BottomCenter">
                 <button
