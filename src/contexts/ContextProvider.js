0ef3232dfd399861f47e0261433c5d680fe4cb41
Linda Paz <lindacsndelapaz@gmail.com>
2023-10-16 11:31:14 -0300

    update ContextProvider

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,35 +1,40 @@
-import React, {createContext, useContext, useState} from "react";
+import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
 const initialState = {
     chat: false,
     cart: false,
-    useProfile: false,
-    notificaton: false,
-}
+    userProfile: false,
+    notification: false,
+};
 
-export const ContextProvider = ({children}) => {
-    const [activeMenu, setActiveMenu] = useState (true);
-    const [isClicked, setIsClicked] = useState (initialState);
-    const [screenSize, setScreenSize] = useState(undefined)
+export const ContextProvider = ({ children }) => {
+    const [screenSize, setScreenSize] = useState(undefined);
+    const [currentColor, setCurrentColor] = useState('#03C9D7');
+    const [currentMode, setCurrentMode] = useState('Light');
+    const [themeSettings, setThemeSettings] = useState(false);
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
 
-    const handleClick = ( clicked ) => {
-        setIsClicked({...initialState, [clicked]: true });
-    }
+    const setMode = (e) => {
+        setCurrentMode(e.target.value);
+        localStorage.setItem('themeMode', e.target.value);
+    };
+
+    const setColor = (color) => {
+        setCurrentColor(color);
+        localStorage.setItem('colorMode', color);
+    };
+
+    const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+
     return (
-        <StateContext.Provider value={{ 
-            activeMenu,
-            setActiveMenu,
-            isClicked, 
-            setIsClicked,
-            handleClick,
-            screenSize, 
-            setScreenSize
-        }}>
-            {children}
+        // eslint-disable-next-line react/jsx-no-constructed-context-values
+        <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
+        {children}
         </StateContext.Provider>
-    )
-}
+    );
+};
 
 export const useStateContext = () => useContext(StateContext);
